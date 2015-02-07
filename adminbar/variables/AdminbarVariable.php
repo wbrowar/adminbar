5634a167bc224817b4884d8b363b296edd43f093
Will Browar <will_browar@dixonschwbl.com>
2015-02-07 18:54:54 -0500

    Css changes

--- adminbar/variables/AdminbarVariable.php
+++ adminbar/variables/AdminbarVariable.php
@@ -10,9 +10,9 @@ class AdminbarVariable
     $newTemplatesPath = craft()->path->getPluginsPath().'adminbar/templates/';
     craft()->path->setTemplatesPath($newTemplatesPath);
     if ($currentEntry != '') {
-      print(craft()->templates->render('index', ['entry' => $currentEntry, 'type' => $type, 'color' => $color]));
+      print(craft()->templates->render('index', ['entry' => $currentEntry, 'color' => $color, 'type' => $type]));
     } else {
-      print(craft()->templates->render('index', ['type' => $type]));
+      print(craft()->templates->render('index', ['color' => $color, 'type' => $type]));
     }
     craft()->path->setTemplatesPath($oldTemplatesPath);
   }
